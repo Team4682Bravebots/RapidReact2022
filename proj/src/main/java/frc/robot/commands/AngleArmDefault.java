13576ff1ca19aca2852712f2273bdcfe48b6a9f1
Frosty72 <72458742+Frosty72@users.noreply.github.com>
2022-02-02 14:41:34 -0800

    Error fix

--- proj/src/main/java/frc/robot/commands/AngleArmDefault.java
+++ proj/src/main/java/frc/robot/commands/AngleArmDefault.java
@@ -44,7 +44,7 @@ public class AngleArmDefault extends CommandBase {
   public void execute() {
     angleArmSubsystem.engageChassis();
     if (timer.hasElapsed(Constants.AngleArmTiming)){
-      angleArmSubsystem.disengageJaws();
+      //angleArmSubsystem.disengageJaws();
       done = true;
     }
   }
